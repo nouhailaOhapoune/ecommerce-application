5ca3913d3c178fdd9d75e8bcf5dcbfaf8bc25878
unknown <mano497>
2022-12-16 14:32:05 +0100

    first commit

---
+++ customer-service/src/main/java/org/sid/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package org.sid.customerservice.repository;
+
+import org.sid.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CustomerRepository  extends JpaRepository<Customer,Long> {
+}
